363e779d611a8ba80ffaddd84852d9fbd9188a16
Mohammed Imran <mohammedimran86992@gmail.com>
2025-05-16 15:15:04 +0530

    Add UserDropDown component for user profile management
    - Introduced a new UserDropDown component to display user avatar and sign-out option.
    - Integrated DropdownMenu for enhanced user interaction.
    - Utilized Avatar component to show user image or initials when image is unavailable.

---
+++ src/components/user-drop-down.tsx
@@ -0,0 +1,38 @@
+import type { Session } from "next-auth";
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SignOutButton } from "./auth/signout-button";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+
+export function UserDropDown({ session }: { session: Session }) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger>
+				<Avatar className="w-10 h-10">
+					<AvatarImage
+						src={session.user?.image || ""}
+						loading="eager"
+						alt={session.user?.name || ""}
+					/>
+					<AvatarFallback>
+						{(
+							session.user?.name?.slice(0, 2) ||
+							session.user?.email?.slice(0, 2) ||
+							"UN"
+						).toUpperCase()}
+					</AvatarFallback>
+				</Avatar>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-42">
+				<DropdownMenuItem>
+					<SignOutButton />
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
