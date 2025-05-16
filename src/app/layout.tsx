dd797bb145941d66b82cbedacae1226d70f0ce3d
Mohammed Imran <mohammedimran86992@gmail.com>
2025-05-16 16:39:42 +0530

    Implement onboarding flow in layout component
    - Added onboarding check to determine if the user has completed the onboarding process based on their email.
    - Integrated the Onboarding component to display for users who are not onboarded, replacing the main content until onboarding is complete.
    - Updated layout structure to conditionally render the Onboarding component or the main children based on the user's session and onboarding status.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,13 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import type React from "react";
 
+import { auth } from "@/auth";
 import { Toaster } from "@/components/ui/sonner";
 import { Navbar } from "@/components/navbar";
 
 import "./globals.css";
+import { Onboarding } from "@/components/onboarding";
+import { checkOnboarding } from "@/actions/check-onboarding";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,12 +22,25 @@ export default async function RootLayout({
 }: {
 	children: React.ReactNode;
 }) {
+	const session = await auth();
+	let isOnboarded = false;
+
+	if (session?.user?.email) {
+		isOnboarded = await checkOnboarding(session.user.email);
+	}
+
 	return (
 		<html lang="en">
 			<body className={inter.className}>
 				<Toaster richColors position="top-center" />
 				<Navbar />
-				<main>{children}</main>
+				{session && isOnboarded === false ? (
+					<main>
+						<Onboarding />
+					</main>
+				) : (
+					<main>{children}</main>
+				)}
 			</body>
 		</html>
 	);
