7d88d70188f89041d8335980e0847c772dbda0c5
Mohammed Imran <mohammedimran86992@gmail.com>
2025-05-17 12:02:40 +0530

    Implement sign-out on onboarding check failure
    - Updated the `checkOnboarding` function to sign out the user if an error occurs while retrieving user data by email.
    - Added error handling for the sign-out process to log any issues encountered during the sign-out attempt.

--- src/actions/check-onboarding.ts
+++ src/actions/check-onboarding.ts
@@ -1,9 +1,13 @@
+import { signOut } from "@/auth";
 import { getUserByEmail } from "@/db/modules/users";
 
 export async function checkOnboarding(email: string) {
 	const [user, err] = await getUserByEmail(email);
 
 	if (err) {
+		await signOut().catch((err) => {
+			console.error(err);
+		});
 		return false;
 	}
 
